ecd6072deffcd4ce6b67148f3026fe161cabe90d
Bruno Wiltemburg <bruno@websix.com.br>
2013-08-01 14:49:54 -0300

    Adicionado exercicio para jquery - uf-cidades

---
+++ jquery/cidades.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Itere sobre o array do estado selecionado no select anterior e exiba as options já formatadas no padrão abaixo:
+ * <option value="Ponta Grossa">Ponta Grossa</option>
+ * OBS: Receba por parâmetro a UF do estado ;)
+ */
+
+
+
+$cidades = array();
+
+//PR
+$cidades['PR'] = 'Ponta Grossa';
+$cidades['PR'] = 'Curitiba';
+$cidades['PR'] = 'Castro';
+$cidades['PR'] = 'Londrina';
+
+//SC
+$cidades['SC'] = 'Florianópolis';
+$cidades['SC'] = 'Camboriú';
+$cidades['SC'] = 'São Joaquim';
+$cidades['SC'] = 'Bombas';
+
+//RS
+$cidades['RS'] = 'Porto Alegre';
+$cidades['RS'] = 'Caxias do Sul';
+$cidades['RS'] = 'Erechim';
+$cidades['RS'] = 'Santa Maria';
---
+++ jquery/ufs.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Itere sobre o array abaixo, mostrando (devolvendo) apenas as options já formatadas no padrão abaixo:
+ * <option value="PR">PR</option>
+ */
+$ufs = array('PR', 'SC', 'RS');
